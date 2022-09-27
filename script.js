9c80f3c485e8fc3d288a1360409f55d3bede3863
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-27 16:46:41 +0600

    geolocation added

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// Fetching geolocation
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert("Sorry! your location is not found.");
+    }
+  );
