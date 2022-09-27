a1c2f590b7874e021fb08a7210aa0c4478523336
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-27 19:53:22 +0600

    leaflet added

--- script.js
+++ script.js
@@ -28,7 +28,20 @@ if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+
+      // Leaflet library
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert("Sorry! your location is not found.");
