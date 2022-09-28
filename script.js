9d3818d7f93e50606b728157ae2c8a8074cbd1b7
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-09-28 11:15:09 +0600

    class constructed

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const months = [
   "December",
 ];
 
-// Elements
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
@@ -30,11 +29,16 @@ class App {
 
   constructor() {
     this._getPosition();
+
+    // Form event handler
     form.addEventListener("submit", this._newWorkout.bind(this));
+
+    // Changing input types
     inputType.addEventListener("change", this._toggleElevationField);
   }
 
   _getPosition() {
+    // Fetching the current location
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
         this._loadMap.bind(this),
@@ -48,12 +52,14 @@ class App {
     const { latitude, longitude } = position.coords;
     const coords = [latitude, longitude];
 
+    // Leaflet library
     this.#map = L.map("map").setView(coords, 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
+    // Map click event handler
     this.#map.on("click", this._showForm.bind(this));
   }
 
@@ -71,6 +77,7 @@ class App {
   _newWorkout(e) {
     e.preventDefault();
 
+    // Clearing fields
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
@@ -79,6 +86,7 @@ class App {
 
     const { lat, lng } = this.#mapEvent.latlng;
 
+    // Map markers
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
